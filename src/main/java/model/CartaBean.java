3b70915bc7ab36d54bea52baed6528f5434c8fb1
samu-ele0 <samuelemetrano@gmail.com>
2026-06-24 18:10:02 +0200

    Aggiunto JavaBean per l'entità Carta

---
+++ src/main/java/model/CartaBean.java
@@ -0,0 +1,99 @@
+package model;
+
+import java.io.Serializable;
+
+public class CartaBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private int punteggio;
+	private String nomeIt;
+	private String nomeEn;
+	private String nomeJp;
+	private String testo;
+	private String pathImg;
+	private String mimeType;
+	
+	public CartaBean() {
+	}
+	
+	public CartaBean(int id, int punteggio, String nomeIt, String nomeEn, String nomeJp, String testo, String pathImg,
+			String mimeType) {
+		super();
+		this.id = id;
+		this.punteggio = punteggio;
+		this.nomeIt = nomeIt;
+		this.nomeEn = nomeEn;
+		this.nomeJp = nomeJp;
+		this.testo = testo;
+		this.pathImg = pathImg;
+		this.mimeType = mimeType;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPunteggio() {
+		return punteggio;
+	}
+
+	public void setPunteggio(int punteggio) {
+		this.punteggio = punteggio;
+	}
+
+	public String getNomeIt() {
+		return nomeIt;
+	}
+
+	public void setNomeIt(String nomeIt) {
+		this.nomeIt = nomeIt;
+	}
+
+	public String getNomeEn() {
+		return nomeEn;
+	}
+
+	public void setNomeEn(String nomeEn) {
+		this.nomeEn = nomeEn;
+	}
+
+	public String getNomeJp() {
+		return nomeJp;
+	}
+
+	public void setNomeJp(String nomeJp) {
+		this.nomeJp = nomeJp;
+	}
+
+	public String getTesto() {
+		return testo;
+	}
+
+	public void setTesto(String testo) {
+		this.testo = testo;
+	}
+
+	public String getPathImg() {
+		return pathImg;
+	}
+
+	public void setPathImg(String pathImg) {
+		this.pathImg = pathImg;
+	}
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+
+	
+}

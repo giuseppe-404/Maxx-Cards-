630d8b986a67f808a80f6ac75f3d8546517d6205
samu-ele0 <samuelemetrano@gmail.com>
2026-06-24 18:39:52 +0200

    Aggiunta Interfaccia Dao per CartaBean

---
+++ src/main/java/dao/CartaDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.CartaBean;
+
+public interface CartaDao {
+
+	public void SaveCarta(CartaBean carta) throws SQLException;
+	
+	public boolean DeleteCarta(int id) throws SQLException;
+	
+	public CartaBean RetriveByKey(int code) throws SQLException;
+	
+	public List<CartaBean> RetrieveAll() throws SQLException;
+	
+	public boolean UpdateId(CartaBean carta, int originalId) throws SQLException;
+	
+	public boolean UpdatePunteggio(CartaBean carta) throws SQLException;
+
+	public boolean UpdateNomeIt(CartaBean carta) throws SQLException;
+	
+	public boolean UpdateNomeEn(CartaBean carta) throws SQLException;
+	
+	public boolean UpdateNomeJp(CartaBean carta) throws SQLException;
+
+	public boolean UpdateTesto(CartaBean carta) throws SQLException;
+
+	public boolean UpdateImage(CartaBean carta) throws SQLException;
+
+}

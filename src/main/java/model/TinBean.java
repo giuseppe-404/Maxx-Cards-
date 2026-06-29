86732ac8fe7f41533e582bd43448e9f269009057
giuseppe-404 <giuseppederosa07@gmail.com>
2026-06-29 22:58:10 +0200

    Aggiunto Bean, Dao e DaoImpl all'entità Tin.

---
+++ src/main/java/model/TinBean.java
@@ -0,0 +1,18 @@
+package model;
+
+public class TinBean extends ConfezionatoBean {
+	private static final long serialVersionUID = 1l;
+	private int id;
+	
+	public TinBean() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}

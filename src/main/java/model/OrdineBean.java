c914d0beba3a2da0db51d8cb1721f4081f8cd99f
giuseppe-404 <giuseppederosa07@gmail.com>
2026-07-06 15:54:07 +0200

    Modificati nomi variabili.

--- src/main/java/model/OrdineBean.java
+++ src/main/java/model/OrdineBean.java
@@ -8,35 +8,35 @@ public class OrdineBean implements Serializable{
 	private static final long serialVersionUID = 1l;
 	
 	private String stato;
-	private int id_ordine;
-	private int id_utente;
-	private int id_metodo;
-	private int id_infosped;
-	private Date data_acquisto;
-	private Date data_consegna;
+	private int idOrdine;
+	private int idUtente;
+	private int idMetodo;
+	private int idInfoSped;
+	private Date dataAcquisto;
+	private Date dataConsegna;
 	
 	public OrdineBean() {}
 
 	public OrdineBean(int id_ordine, int id_utente, int id_metodo, int id_infosped) {
 		this.stato = "Carrello";
-		this.id_ordine = id_ordine;
-		this.id_utente = id_utente;
-		this.id_metodo = id_metodo;
-		this.id_infosped = id_infosped;
-		this.data_acquisto = null;
-		this.data_consegna = null;
+		this.idOrdine = id_ordine;
+		this.idUtente = id_utente;
+		this.idMetodo = id_metodo;
+		this.idInfoSped = id_infosped;
+		this.dataAcquisto = null;
+		this.dataConsegna = null;
 	}
 
 	public OrdineBean(String stato, int id_ordine, int id_utente, int id_metodo, int id_infosped, Date data_acquisto,
 			Date data_consegna) {
 		super();
 		this.stato = stato;
-		this.id_ordine = id_ordine;
-		this.id_utente = id_utente;
-		this.id_metodo = id_metodo;
-		this.id_infosped = id_infosped;
-		this.data_acquisto = data_acquisto;
-		this.data_consegna = data_consegna;
+		this.idOrdine = id_ordine;
+		this.idUtente = id_utente;
+		this.idMetodo = id_metodo;
+		this.idInfoSped = id_infosped;
+		this.dataAcquisto = data_acquisto;
+		this.dataConsegna = data_consegna;
 	}
 
 	public String getStato() {
@@ -48,50 +48,50 @@ public class OrdineBean implements Serializable{
 	}
 
 	public int getIdOrdine() {
-		return id_ordine;
+		return idOrdine;
 	}
 
 	public void setIdOrdine(int id_ordine) {
-		this.id_ordine = id_ordine;
+		this.idOrdine = id_ordine;
 	}
 
 	public int getIdUtente() {
-		return id_utente;
+		return idUtente;
 	}
 
 	public void setIdUtente(int id_utente) {
-		this.id_utente = id_utente;
+		this.idUtente = id_utente;
 	}
 
 	public int getIdMetodo() {
-		return id_metodo;
+		return idMetodo;
 	}
 
 	public void setIdMetodo(int id_metodo) {
-		this.id_metodo = id_metodo;
+		this.idMetodo = id_metodo;
 	}
 
 	public int getIdInfoSped() {
-		return id_infosped;
+		return idInfoSped;
 	}
 
 	public void setIdInfoSped(int id_infosped) {
-		this.id_infosped = id_infosped;
+		this.idInfoSped = id_infosped;
 	}
 
 	public Date getDataAcquisto() {
-		return data_acquisto;
+		return dataAcquisto;
 	}
 
 	public void setDataAcquisto(Date data_acquisto) {
-		this.data_acquisto = data_acquisto;
+		this.dataAcquisto = data_acquisto;
 	}
 
 	public Date getDataConsegna() {
-		return data_consegna;
+		return dataConsegna;
 	}
 
 	public void setDataConsegna(Date data_consegna) {
-		this.data_consegna = data_consegna;
+		this.dataConsegna = data_consegna;
 	}
 }

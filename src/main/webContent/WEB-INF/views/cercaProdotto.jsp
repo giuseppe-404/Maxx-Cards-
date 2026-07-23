cd46f6cbc4d59a7144449620408c303d47294236
samu-ele0 <samuelemetrano@gmail.com>
2026-07-23 18:38:37 +0200

    Creata JSP per ricercare prodotti

---
+++ src/main/webContent/WEB-INF/views/cercaProdotto.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Cerca Prodotto</title>
+<%@include file="/WEB-INF/views/components/ext.jsp"%>
+<script src="${pageContext.request.contextPath}/scripts/cerca_prodotto.js"></script>
+<link href="${pageContext.request.contextPath}/styles/cerca_prodotto.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<%@ include file="/WEB-INF/views/components/header.jsp"%>
+	<%@include file="/WEB-INF/views/components/optionsAside.jsp"%>
+	<section id="ricerca_wrapper">
+		<details open>
+			<summary>Impostazioni di ricerca</summary>
+			<form action="">
+				<fieldset>
+					<input type="hidden" id="search_category" value="0">
+					<label for="nome">Nome:
+						<input type="text" name="nome" id="nome">
+					</label>
+					<label for="descrizione">Descrizione:
+						<input type="text" name="descrizione" id="descrizione">
+					</label>
+					<label for="prezzo">Prezzo:
+						<input type="number" name="prezzo" id="prezzo">
+					</label>
+					<input type="button" id="cerca_prod" value="Ricerca">
+				</fieldset>
+			</form>
+		</details>
+		<main>
+			<p class="hidden" id="fail">Nessun prodotto trovato</p>
+			<section class="product_selection" id="search_result">
+				<a href="getProdottoPage?id=0" class="product_link selezionabile hidden">
+					<section class="product_sale hidden">
+						<p>Sconto!</p>
+						<p>10%</p>
+					</section>
+					<section class="product_image">
+						<img src="" alt="Immagine di x">
+					</section>
+					<section class="product_description">
+						<h3>nome</h3>
+						<p>desc</p>
+					</section>
+				</a>
+			</section>
+		</main>
+	</section>
+	<%@include file="/WEB-INF/views/components/footer.jsp"%>
+</body>
+</html>
---
+++ src/main/webContent/scripts/cerca_prodotto.js
@@ -0,0 +1,78 @@
+window.addEventListener("load", function() {
+    HTMLCollection.prototype.forEach = Array.prototype.forEach;
+
+	const search_category = document.getElementById("search_category");
+    const cerca = document.getElementById("cerca_prod");
+    const nome = document.getElementById("nome");
+    const descrizione = document.getElementById("descrizione");
+    const prezzo = document.getElementById("prezzo");
+    const lingua = document.getElementById("lingua");
+    const idSet = document.getElementById("idSet");
+	const fail = document.getElementById("fail");
+	const template = document.getElementsByClassName("product_link")[0];
+	
+    if (cerca) {
+        var param = "idProdotto="+search_category.value;
+        if (nome && nome.value)
+            param += "&nome="+nome.value
+        if (descrizione && descrizione.value)
+            param += "&descrizione="+descrizione.value
+        if (prezzo && prezzo.value)
+            param += "&prezzo="+prezzo.value
+        if (lingua && lingua.value)
+            param += "&lingua="+lingua.value
+        if (idSet && idSet.value)
+            param += "&idSet="+idSet.value
+        /*nome, descrizione, prezzo, lingua, idSet, quality, idProdotto (
+            null o 0 > prodotot
+            1 > carta sing
+            2 > prod yugi 
+            3 > confezionato
+            4 > pacchetto
+            5 > tin
+            6 > box
+            7 > structure
+            8 > deck
+        */
+        cerca.addEventListener("click", function() {
+            ajax("servletCercaProdottoJson", "post", param, function(request){
+				if (request.readyState < 4)
+					return
+				if (request.status != 200)
+					return
+				
+				var arr = JSON.parse(request.responseText);
+				if(arr.length == 0){
+					fail.classList.remove("hidden");
+					return
+				}
+				fail.classList.add("hidden");
+				
+				var children = [template]
+				arr.forEach(function(prod){
+					var clone = template.cloneNode(true);
+					clone.classList.remove("hidden")
+					children.push(clone);
+					
+					clone.href = "getProdottoPage?id="+prod.id;
+					
+					if (prod.sconto>0){
+						var product_sale = clone.getElementsByClassName("product_sale")[0];
+						product_sale.classList.remove("hidden");
+						product_sale.getElementsByTagName("p")[1].innerHTMl = (prod.sconto/100) + "%";
+					}
+					
+					clone.getElementsByTagName("img")[0].src="image?action=show&prodottoId="+prod.id+"&isProdotto=true"
+					clone.getElementsByTagName("img")[0].alt="immagine di"+prod.nome;
+					
+					var product_description = clone.getElementsByClassName("product_description")[0];
+					product_description.getElementsByTagName("h3")[0].innerHTMl = prod.nome;
+					product_description.getElementsByTagName("p")[0].innerHTMl = prod.descrizione;
+				})
+				template.parentNode.replaceChildren(...children);
+			})
+        })
+		
+		cerca.click();
+    }
+})
